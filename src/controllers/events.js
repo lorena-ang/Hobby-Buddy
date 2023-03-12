ec34ccc6e834eec8f002e4c76c2552db02066fa7
lorena-ang <loreang98@hotmail.com>
2023-03-12 03:10:24 -0600

    Vista detalle de evento

--- src/controllers/events.js
+++ src/controllers/events.js
@@ -3,18 +3,20 @@ const { isLoggedIn, isEventAuthor } = require('../middleware');
 const { handle } = require('../utilis');
 
 const Event = require('../models/event');
+const Comment = require('../models/comment');
 
 var router = express.Router();
 
 router.get('/', async (req, res) => {
-    const [events, e] = await handle(Event.find());
+    const [events, e] = await handle(Event.find().populate('organizer'));
+    console.log(events);
 
     if (e) {
         console.log(e);
         return;
     }
 
-    res.render('events/list', { page: 'principal', events: events });
+    res.render('events/list', { page: 'principal', events });
 });
 
 router.get('/crear', isLoggedIn, (req, res) => {
@@ -24,7 +26,7 @@ router.get('/crear', isLoggedIn, (req, res) => {
 router.post('/crear', isLoggedIn, async (req, res) => {
     console.log(req.body.event);
     const event = new Event(req.body.event);
-    event.author = req.user._id;
+    event.organizer = req.user._id;
 
     const [new_event, e] = await handle(event.save());
 
@@ -36,5 +38,17 @@ router.post('/crear', isLoggedIn, async (req, res) => {
     console.log('Evento creado');
     res.redirect(`/eventos/${new_event._id}`);
 });
+
+router.get('/:id', async (req, res) => {
+    let [event, e] = await handle(Event.findOne({ _id: req.params.id }).populate(['organizer']).populate({ path: 'comments', populate: { path: 'author' }, options: { sort: { 'createdAt': 'desc' } } }).exec());
+    console.log(event);
+    if (e || event === null) {
+        console.log(e);
+        return;
+    }
+
+    res.render('events/detail', { event, user: req.user });
+});
+
 
 module.exports = router;
